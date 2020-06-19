1d1041569ac71c2064710e8654109ddf796e62de
JCathey10 <catheyj@my.lanecc.edu>
2020-06-18 17:18:18 -0700

    Fixed issues with google apis, input bar now loads google places

--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
-const searchElement = document.querySelector('[data-city-search]')
+const searchElement = document.querySelector('[data-city-search]');
+const searchBox = new google.maps.places.SearchBox(searchElement);
+searchBox.addListener('places_changed', () => {
+    // assign the selected place
+    const place = searchBox.getPlaces()[0]
+    // don't do anything if user hasn't selected a place
+    if (place == null) return;
+    
+    // get longitude and latitude
+    const latitude = place.geometry.location.lat();
+    const longitude = place.geometry.location.lng();
+
+    // get the location's weather data
+    fetch('/weather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            latitude: latitude,
+            longitude: longitude
+        })
+    }).then(res => res.json()).then(data => {
+        // set the weather data and the selected address
+        setWeatherData(data, place.formatted_address);
+    }) 
+})
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 if (process.env.NODE_ENV != 'production') {
-    require('dotenv').config()
+    require('dotenv').config();
 }
 
-const ACCUWEATHER_API_KEY = process.env.ACCUWEATHER_API_KEY
-const express = require('express')
-const app = express()
+const ACCUWEATHER_API_KEY = process.env.ACCUWEATHER_API_KEY;
+const express = require('express');
+const app = express();
 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/weather', (req, res) => {
-
+    console.log(req.body);
 })
 
 app.listen(3000, () => {
-    console.log('Server Started')
+    console.log('Server Started');
 })
